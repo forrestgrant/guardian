d1faba7dbd6c991e21bdc727126bc71d8eb0be39
Forrest Grant <fgrant@atlantistech.com>
2013-01-24 14:52:29 -0500

    adding js files back

--- lib/guardian.rb
+++ lib/guardian.rb
@@ -2,17 +2,21 @@ require "guardian/version"
 require 'guardian/engine'
 
 module Guardian
+
   extend ActiveSupport::Concern
-    
-    def guard(vars = { :threshold => 2})
-    	if guarded?(params)
-    		redirect_to "/" unless params[:duration].to_i > vars[:threshold]
-    	end
-    end
 
-    def guarded?(params)
-    	params.keys.include?('duration')
-    end
-
+  included do
+    hide_action :guard
   end
+    
+  def guard(vars = { :threshold => 2})
+  	if guarded?(params)
+  		redirect_to "/" unless params[:duration].to_i > vars[:threshold]
+  	end
+  end
+
+  def guarded?(params)
+  	params.keys.include?('duration')
+  end
+
 end
